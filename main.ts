48d8c4477caa34f4a54d6bc1cec5cd8c019165a9
flmosquera <83605925+flmosquera@users.noreply.github.com>
2021-05-03 13:43:53 -0500

    Nombre de un animal

--- main.ts
+++ main.ts
@@ -1,3 +1,51 @@
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    # # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    # . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.showLeds(`
+    . # . . .
+    # # . . .
+    # . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(3000)
+basic.showLeds(`
+    . # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(5000)
+basic.showLeds(`
+    . # . . .
+    . # . . .
+    # # . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
 basic.forever(function () {
 	
 })
